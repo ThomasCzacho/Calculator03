40414dea137b8aad1fe59f9ab14436e54b9f6f57
ThomasCzacho <Thomas.Czachorowski@cdpipelinedevshop.com>
2022-10-18 17:24:59 -0400

    Initial commit

---
+++ src/Calculator3.java
@@ -0,0 +1,12 @@
+public class Calculator3 {
+    public double tax = .05;
+    public double tip = .15;
+    public double originalPrice = 0;
+    public String name;
+
+    public void findTotal(double price, String name){
+        double total = price*(1+tax+tip);
+        System.out.println(name+"'s total: "+total);
+
+    }
+}
